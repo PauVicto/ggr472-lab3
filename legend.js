e2977bd168a6bfc269a202118d2d8a9226154091
PauVicto <victoria.pau@mail.utoronto.ca>
2026-03-07 19:38:14 -0500

    added in css for legend and src images for legend

--- legend.js
+++ legend.js
@@ -1,7 +1,7 @@
 const legenditems = [
-    { category: 'Cafe', icon: 'cafe' },
-    { category: 'Restaurant', icon: 'restaurant' },
-    { category: 'Study Spot', icon: 'star' },
+    { category: 'Cafe', icon: 'icons/cafe-icon.png' },
+    { category: 'Restaurant', icon: 'icons/restaurant-icon.png' },
+    { category: 'Study Spot', icon: 'icons/star-icon.png' },
 ];
 
 const legend = document.getElementById('legend');
@@ -9,10 +9,10 @@ const legend = document.getElementById('legend');
 
 legenditems.forEach(({category, icon}) => {
     const row = document.createElement('div');
-    const iconSymbol = document.createElement('span');
+    const iconSymbol = document.createElement('img');
     
     iconSymbol.className = 'legend-icon';
-    iconSymbol.textContent = icon;
+    iconSymbol.src = icon;
 
     const text = document.createElement('span');
     text.textContent = category;
